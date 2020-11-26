f40ffd9cc56a0cef5015632768d7906721adb36b
Todd Hendricks <hendricks.ta@gmail.com>
2020-11-26 13:12:47 -0800

    add generate orders and generate report classes

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import luigi
+from luigi import Task, LocalTarget
+# class SayHello(Task):
+#     def output(self):
+#         return luigi.LocalTarget('result.csv')
+#     def run(self):
+#         print('hello world')
+#         with self.output().open('w') as f:
+#             f.write('ok')
 
-class SayHello(luigi.Task):
+class ProcessOrders(Task):
     def output(self):
-        return luigi.LocalTarget('result.csv')
+        return LocalTarget('orders.csv')
     def run(self):
-        print('hello world')
         with self.output().open('w') as f:
-            f.write('ok')
+            print('May, 100', file=f)
+            print('May, 180', file=f)
+            print('June, 200', file=f)
+            print('June, 150', file=f)
+
+class GenerateReport(Task):
+    def requires(self):
+        return ProcessOrders()
+    def output(self):
+        return LocalTarget('report.csv')
+    def run(self):
+        report = {}
+        for line in self.input().open():
+            month, amount = line.split(',')
+            if month in report:
+                report[month] += float(amount)
+            else:
+                report[month] = float(amount)
+        with self.output().open('w') as out:
+            for month in report:
+                print(month + ',' + str(report[month]), file=out)
+
+
 
 # class MyTask(luigi.Task):
 #     x = luigi.IntParameter()
@@ -15,8 +44,5 @@ class SayHello(luigi.Task):
 #     def run(self):
 #         print(self.x + self.y)
 
-
-def print_hi(name):
-    print("hi " + str(name))
 if __name__ == '__main__':
-    luigi.run(['SayHello', '--local-scheduler'])
+    luigi.run(['GenerateReport', '--local-scheduler'])
